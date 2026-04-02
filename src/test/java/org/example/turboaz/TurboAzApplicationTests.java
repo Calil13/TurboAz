d924c7c89cafea44375664f33e2bed68da7d8f65
Calil <ahmadovcalil13@gmail.com>
2026-04-02 17:24:48 +0400

    First commit

---
+++ src/test/java/org/example/turboaz/TurboAzApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.turboaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurboAzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
